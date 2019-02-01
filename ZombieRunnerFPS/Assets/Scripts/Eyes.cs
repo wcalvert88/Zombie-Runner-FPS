e5291173c642cbb3c63e1b55d807c30e035b4bcd
Wade Calvert <wcalvert88@gmail.com>
2019-02-01 10:45:35 -0500

    organized some stuff removed some unused scenes and got zoom and call helicopter to work

---
+++ ZombieRunnerFPS/Assets/Scripts/Eyes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour
+{
+    private Camera eyes;
+    private float defaultFOV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eyes = GetComponent<Camera>();
+        defaultFOV = eyes.fieldOfView;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Zoom")) {
+            eyes.fieldOfView = defaultFOV / 1.5f;
+            Debug.Log("v pressed");
+        } else {
+            eyes.fieldOfView = defaultFOV;
+            Debug.Log("v released");
+        }
+    }
+}
---
+++ ZombieRunnerFPS/Assets/Scripts/Helicopter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    public AudioClip callSound;
+    private bool called = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("CallHeli") && !called) {
+            // Call the helicopter
+            called = true;
+            // Play sound
+            print ("Helicopter called");
+        }
+    }
+}
